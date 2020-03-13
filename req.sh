78e6678335845de0bab61193c7d2cfc92ffdd7fa
inutile2014 <in.utile2014@gmail.com>
2020-03-13 13:24:04 +0100

    Create req.sh

---
+++ req.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3-pip -y
+sudo apt-get install terminator -y
+sudo apt-get install torghost -y
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+sudo apt-get install openjdk-8-jre -y
